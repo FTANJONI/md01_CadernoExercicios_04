4a6c8c4063ba48cf21d675f47aab09489853c373
Fabricio Tanjoni <fabriciotanjoni@gmail.com>
2022-08-23 21:24:14 -0300

    Exercicio4

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace md01_CadernoExercicios_04
+{
+    class Program
+        /*4. Faça um algoritmo que leia uma temperatura em graus Celsius e apresente-a convertida em graus
+        Fahrenheit. A fórmula de conversão é: F = (9 * C + 160) / 5,
+        na qual F é a temperatura em Fahrenheit e C é a temperatura em Celsius
+        */
+    {
+        static void Main(string[] args)
+            
+        {
+            double c, f;
+            Console.WriteLine("Digite a Temperatura Celsius para converte em Fahrenheit:");
+            c = Convert.ToDouble(Console.ReadLine());
+            f = (9 * c + 160) / 5;
+
+            Console.WriteLine("\nTemperatura informada: {0}" , c.ToString("0.0") + "°C");
+            Console.WriteLine("\nTemperatura convertida para Fahrenheit: {0}" , f.ToString("0.0") + "°F");
+        }
+    }
+}
